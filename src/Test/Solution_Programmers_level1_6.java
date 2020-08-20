6de52bc5bb99aa28fac8ac96cf0577f4835e8f9f
MinAh Jo <48617981+mina0774@users.noreply.github.com>
2020-08-20 16:55:37 +0900

    2016년

--- src/Test/Solution_Programmers_level1_6.java
+++ src/Test/Solution_Programmers_level1_6.java
@@ -2,12 +2,15 @@ package Test;
 import java.util.*;
 public class Solution_Programmers_level1_6 {
 
+	// 1-12월 전체 달의 일수
 	    int day_num[]={31,29,31,30,31,30,31,31,30,31,30,31};
 	    String day[]={"SUN","MON","TUE","WED","THU","FRI","SAT"};
 	    public String solution(int a, int b) {
 	    
 	        String answer = "";
+		    //금요일을 나타내는 인덱스
 	        int today_num=5;
+		    // 전체 일수
 	        int total_day=0;
 
 	        for(int i=0;i<a-1;i++){
