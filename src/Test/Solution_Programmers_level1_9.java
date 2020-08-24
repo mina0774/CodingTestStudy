b598b209600c079fd42a3897968ef05b62d30bf0
jomin-a <jomin-a@localhost>
2020-08-24 14:39:22 +0900

    같은 숫자는 싫어!

--- src/Test/Solution_Programmers_level1_9.java
+++ src/Test/Solution_Programmers_level1_9.java
@@ -1,5 +1,23 @@
 package Test;
-
+import java.util.*;
 public class Solution_Programmers_level1_9 {
-
+	 public int[] solution(int []arr) {
+	        
+	        ArrayList<Integer> tempList=new ArrayList<Integer>();
+	        int prenum=arr[0];
+	        tempList.add(arr[0]);
+	        
+	        for (int num : arr){
+	            if(prenum!=num){
+	                tempList.add(num);
+	            }
+	            prenum=num;
+	        }
+	        int[] answer=new int[tempList.size()];
+	        for (int i=0; i<answer.length;i++){
+	            answer[i]=tempList.get(i).intValue();
+	        }
+	        
+	        return answer;
+	    }
 }
