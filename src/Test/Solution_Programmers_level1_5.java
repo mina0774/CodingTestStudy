0361d6760135f8828ef48d8f06b618070e29b500
MinAh Jo <48617981+mina0774@users.noreply.github.com>
2020-08-24 13:56:44 +0900

    가운데 글자 가져오기

--- src/Test/Solution_Programmers_level1_5.java
+++ src/Test/Solution_Programmers_level1_5.java
@@ -4,16 +4,15 @@ import java.util.*;
 public class Solution_Programmers_level1_5 {
 	   public String solution(String s) {
 	        String answer = "";
-	        if(s.length()%2==0){
+	        if(s.length()%2==0){ //글자수가 짝수일 때
 	            int a=s.length()/2;
 	            answer=s.substring(a-1,a+1);
-	        }else{
+	        }else{ //글자수가 홀수일 때
 	            int a=s.length()/2;
 	            answer=s.substring(a,a+1);
 	        }
 	        return answer;
 	        
-	        // �ٸ� Ǯ��
-	        // return s.substring((word.length()-1) / 2, word.length()/2 + 1);    
+	    
 	    }
 }
