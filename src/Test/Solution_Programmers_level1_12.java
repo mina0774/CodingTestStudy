17dba5913d829a3700fdfd2f5cdf77be3db8393e
mina0774 <mina0774@hanmail.net>
2020-08-31 18:24:24 +0900

    문자열 내 p와 y의 개수

--- src/Test/Solution_Programmers_level1_12.java
+++ src/Test/Solution_Programmers_level1_12.java
@@ -1,5 +1,24 @@
 package Test;
+import java.util.*;
 
 public class Solution_Programmers_level1_12 {
 
+	    public String[] solution(String[] strings, int n) {
+	        String[] answer = {};
+	        
+	        // 배열을 한바퀴 돌면서 특정 인덱스에 해당하는 문자값을 문자열 앞에 붙여줌
+	        for(int i=0;i<strings.length;i++){
+	            strings[i]=strings[i].charAt(n)+strings[i];
+	        }
+	        
+	        // 앞글자에 특정 인덱스 문자값을 붙여줬으므로 사전 순으로 sorting
+	        Arrays.sort(strings);
+	        
+	        answer=new String[strings.length];
+	        
+	        for(int i=0;i<strings.length;i++){
+	            answer[i]=strings[i].substring(1,strings[i].length());
+	        }
+	        return answer;
+	    }
 }
