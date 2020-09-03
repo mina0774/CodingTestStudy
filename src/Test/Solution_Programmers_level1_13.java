981d6cb8d657330a2d903e76b3b9fed077d67bf1
mina0774 <mina0774@hanmail.net>
2020-09-03 20:50:19 +0900

    p와 y의 개수

--- src/Test/Solution_Programmers_level1_13.java
+++ src/Test/Solution_Programmers_level1_13.java
@@ -1,5 +1,28 @@
 package Test;
+import java.util.*;
 
 public class Solution_Programmers_level1_13 {
+	  boolean solution(String s) {
+	        boolean answer = true;
 
+	        int count_p=0; // p 개수
+	        int count_y=0; // y 개수
+	        
+	        for(int i=0; i<s.length(); i++){
+	            if(s.charAt(i)=='p'||s.charAt(i)=='P'){
+	                count_p++;
+	            }
+	            if(s.charAt(i)=='y'||s.charAt(i)=='Y'){
+	                count_y++;
+	            }
+	        }
+	        
+	        if(count_p==count_y){
+	            answer=true;
+	        }else{
+	            answer=false;
+	        }
+
+	        return answer;
+	    }
 }
