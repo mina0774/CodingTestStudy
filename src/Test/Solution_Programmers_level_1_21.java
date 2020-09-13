cf6cbaf1e93bd91083961c84d22c8b8fac29d3d4
mina0774 <mina0774@hanmail.net>
2020-09-13 22:08:58 +0900

    약수의 합

--- src/Test/Solution_Programmers_level_1_21.java
+++ src/Test/Solution_Programmers_level_1_21.java
@@ -1,5 +1,13 @@
 package Test;
 
 public class Solution_Programmers_level_1_21 {
-
+	public int solution(int n) {
+        int answer = 0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){ 
+                answer+=i;
+            }
+        }
+        return answer;
+    }
 }
