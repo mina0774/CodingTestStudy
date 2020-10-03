e7350ab6db566d12f0f8b735fd32705c3b679420
mina0774 <mina0774@hanmail.net>
2020-10-03 20:34:49 +0900

    짝수와 홀수

--- src/Test/Solution_Programmers_level1_29.java
+++ src/Test/Solution_Programmers_level1_29.java
@@ -1,5 +1,14 @@
 package Test;
+import java.util.*;
 
 public class Solution_Programmers_level1_29 {
-
+    public String solution(int num) {
+        String answer = "";
+        if(num%2==0){
+            answer="Even";
+        }else{
+            answer="Odd";
+        }
+        return answer;
+    }
 }
