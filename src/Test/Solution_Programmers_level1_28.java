7274f5f50e0aa18700f5204921b7842a3fbb14fb
mina0774 <mina0774@hanmail.net>
2020-10-03 20:28:19 +0900

    제일 작은 수 제거하기

--- src/Test/Solution_Programmers_level1_28.java
+++ src/Test/Solution_Programmers_level1_28.java
@@ -1,5 +1,30 @@
 package Test;
+import java.util.*;
 
 public class Solution_Programmers_level1_28 {
-
+	 public int[] solution(int[] arr) {
+	        int[] answer = {};
+	       
+	        if(arr.length==1){ // 배열의 길이가 1일 때, -1을 가지고 있는 배열을 return
+	            return new int[]{-1};
+	        }else{
+	            int min=arr[0];
+	            for(int i=0;i<arr.length;i++){ // 배열의 최솟값을 찾아서~
+	                if(arr[i]<min)
+	                    min=arr[i];
+	            } 
+	            
+	            answer=new int[arr.length-1];
+	            int j=0;
+	            for(int i=0;i<arr.length;i++){ // 배열 최솟값만 뺀 배열을 만들기
+	                if(arr[i]==min){
+	                    continue;
+	                }else{
+	                    answer[j++]=arr[i];
+	                }              
+	            }
+	            return answer;        
+	        }
+	        
+	    }
 }
