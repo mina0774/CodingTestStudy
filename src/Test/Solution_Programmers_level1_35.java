6fbb8aff8f37dc2392a560c751f219b1ba48bac0
mina0774 <mina0774@hanmail.net>
2020-10-09 02:23:33 +0900

    핸드폰 번호 가리기

--- src/Test/Solution_Programmers_level1_35.java
+++ src/Test/Solution_Programmers_level1_35.java
@@ -1,5 +1,19 @@
 package Test;
 
 public class Solution_Programmers_level1_35 {
-
+	   public String solution(String phone_number) {
+	        String answer = "";
+	        String[] arr=phone_number.split("");
+	        
+	        // 뒷자리 4개 앞까지만 *로 변경
+	        for(int i=0;i<arr.length-4;i++){
+	            arr[i]="*";
+	        }
+	        
+	        for(int i=0;i<arr.length;i++){
+	            answer+=arr[i];
+	        }
+	        
+	        return answer;
+	    }
 }
