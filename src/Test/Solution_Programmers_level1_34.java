77aa95472996fb3550282bc3ff00c1696aecaf31
mina0774 <mina0774@hanmail.net>
2020-10-09 02:18:33 +0900

    하샤드 수

--- src/Test/Solution_Programmers_level1_34.java
+++ src/Test/Solution_Programmers_level1_34.java
@@ -1,5 +1,19 @@
 package Test;
 
 public class Solution_Programmers_level1_34 {
-
+    public boolean solution(int x) {
+        int sum = 0;
+		int a = x;
+      
+        // 모든 자릿수 더하기
+		while (a >= 1) {
+			sum += a % 10;
+			a /= 10;
+		}
+ 
+		if (x % sum == 0) {
+			return true;
+		} else
+			return false;
+    }
 }
