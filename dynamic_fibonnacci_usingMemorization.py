50b7cc2c291b278dce1cc47335095a61bc8503af
mina0774 <mina0774@hanmail.net>
2021-01-22 14:43:22 +0900

    dynamic programming - 메모제이션을 이용한 피보나치 수열 구현

---
+++ dynamic_fibonnacci_usingMemorization.py
@@ -0,0 +1,17 @@
+# 한 번 계산된 결과를 메모제이션(memoization)하기 위한 리스트 초기화
+d=[0]*100
+
+# 피보나치 함수를 재귀함수로 구현 (탑다운 다이나믹 프로그래밍)
+def fibo(x):
+    if x==1 or x==2:
+        return 1
+
+    # 이미 계산한 적이 있는 항이라면 그대로 반환
+    if d[x]!=0:
+        return d[x]
+
+    d[x]=fibo(x-1)+fibo(x-2)
+
+    return d[x]
+
+print(fibo(99))
