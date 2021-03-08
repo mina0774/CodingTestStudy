52d6ce33a991ba0eda9298ee2f92c4579a54d1c5
mina0774 <mina0774@hanmail.net>
2021-03-09 01:00:08 +0900

    구현 문제 - 기둥, 보 설치

--- q12_programmers_60061.py
+++ q12_programmers_60061.py
@@ -50,7 +50,4 @@ def solution(n, build_frame):
                 answer.remove([x,y,stuff])
     return sorted(answer)
 
-
-    return answer
-
 print(solution(5,[[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]))
