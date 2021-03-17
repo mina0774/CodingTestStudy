062f3fe3f21e7c10f71b477eb61d6ae6cab52a34
mina0774 <mina0774@hanmail.net>
2021-03-17 16:32:44 +0900

    백준 - 사다리 조작 (완전 탐색)

--- baekjoon_15684.py
+++ baekjoon_15684.py
@@ -1,51 +1,58 @@
-# brute force 문제 사다리 조작 (백준 15684)
-# 시간초과~~~~~~~~~ 나중ㅇ에 해보자아아ㅏㅏ]
-res=10000
+# 사다리조작
+# 완전 탐색 (BruteForce)
+
+from sys import exit
+
 n,m,h=map(int,input().split())
 
-graph=[[0]*n for _ in range(h)]
+if m==0:
+    print(0)
+    exit()
+
+bridge=[[False]*n for _ in range(h)]
+
 for _ in range(m):
     a,b=map(int,input().split())
-    graph[a-1][b-1]=1
+    bridge[a-1][b-1]=True
 
-# 사다리가 모두 자기 자신으로 돌아가는지 확인하는 함수
+# i번째 세로선의 결과가 i가 나오는지 확인하는 함수
 def check():
-    for i in range(n):
-        start=i
-        for j in range(h):
-            # 사다리가 현재를 기준점으로 오른쪽에 있을 경우
-            if graph[j][start]:
-                start+=1
-            # 사다리가 현재를 기준점으로 왼쪽에 있을 경우
-            elif start-1 >=0 and graph[j][start-1]:
-                start-=1
-        if start!=i:
+    for start in range(n):
+        k=start
+        for i in range(h):
+            if bridge[i][k]==True:
+                k+=1
+            elif k>0 and bridge[i][k-1]==True:
+                k-=1
+        if start!=k:
             return False
     return True
 
-def bf(start,cnt):
-    global res
-    if cnt<=3:
-        if check():
-            res=min(res,cnt)
-            return
+def brute_force(cnt,x,y):
+    global ans
+    if check():
+        ans=min(ans,cnt)
+        return
+    # i번째에 도달하지 않고, 현재 cnt가 3이거나 ans값을 초과하면, 종료
+    elif cnt==3 or ans<=cnt:
+        return
 
-    for i in range(start,h):
-        for j in range(n-1):
-            # 전후로 놓인 사다리가 없고, 현재 위치도 사다리가 놓여있지 않다면
-            if not graph[i][j]+graph[i][j-1]+graph[i][j+1]:
-                graph[i][j]=1
-                bf(i,cnt+1)
-                graph[i][j]=0
+    for i in range(x,h):
+        if i==x:
+            k=y
+        else:
+            k=0
 
-bf(0,0)
-if res!=10000:
-    print(res)
+        for j in range(k,n-1):
+            if not bridge[i][j] and not bridge[i][j+1]:
+                bridge[i][j]=True
+                brute_force(cnt+1,i,j+2)
+                bridge[i][j]=False
+
+# ans의 최대값은 3이므로 4로 초기화해주고 시작
+ans=4
+brute_force(0,0,0)
+if ans<4:
+    print(ans)
 else:
     print(-1)
-
-
-
-
-
-
